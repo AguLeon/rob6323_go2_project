11d99f0005b4ea8308fc7dd7086988a2fbcdb92a
AguLeon <leon.agustin@gmail.com>
2025-12-19 11:48:23 -0500

    Prepare for Run_10

--- source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env_cfg.py
+++ source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env_cfg.py
@@ -95,9 +95,10 @@ class Rob6323Go2EnvCfg(DirectRLEnvCfg):
     current_vel_visualizer_cfg.markers["arrow"].scale = (0.5, 0.5, 0.5)
 
     # reward scales
-    lin_vel_reward_scale = 3.0
-    yaw_rate_reward_scale = 1.5
-    action_rate_reward_scale = -0.05
+    lin_vel_reward_scale = 16.0
+    yaw_rate_reward_scale = 8.0
+    action_rate_reward_scale = -0.1
+
     # Additional reward scales
     orient_reward_scale = -5.0
     lin_vel_z_reward_scale = -0.02
